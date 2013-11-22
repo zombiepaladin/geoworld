019d8fa324b42b9f2f13f735aa905220c51d6d6e
= <nhb7817@ksu.edu>
2013-11-22 04:55:28 -0600

    Cleaned up pipeline code and added manifest file

--- make.js
+++ make.js
@@ -1,29 +1,25 @@
 var fs = require('fs'),
-	UglifyJS = require("uglify-js"),
-	CleanCSS = require("clean-css")
-	
+	uglifyJS = require("uglify-js"),
+	cleanCSS = require("clean-css"),
+	manifest = require("./manifest.json");
+
 // ==========================================
 // JavaScript pre-processing
 //-------------------------------------------	
 
 // Combine JavaScript files with uglify-js
+// File list is pulled from the manifest.json file
 // Be sure to list files in order of inclusion
 // to avoid initialization issues
-var minifiedJS = UglifyJS.minify([
-	"src/js/utilities.js",
-	"src/js/animation_polyfill.js",
-	"src/js/math_helper_library.js",
-	"src/js/vector_library.js",
-	"src/js/dynamicPhysicsObject.js",
-	"src/js/player.js",
-	"src/js/geoworld.js"
-]);
+var minifiedJS = uglifyJS.minify(manifest.javascripts);
 
 // Write combined, minified, JavaScript file to release directory
 fs.writeFile('release/geoworld.js', minifiedJS.code, function(err) {
 	if(err) {
-		console.log("Could not write release/geoworld.js file\n" + err);
+		console.error("Could not write release/geoworld.js file\n" + err);
+		return;
 	}
+	console.log("wrote file: release/geoworld.js");
 });
 
 
@@ -31,17 +27,22 @@ fs.writeFile('release/geoworld.js', minifiedJS.code, function(err) {
 // CSS pre-processing
 //----------------------------------------------
 
-// Read CSS from file (currently only geoworld.css)
-var cssSource = fs.readFileSync('src/css/geoworld.css', 'utf8');
+// Read CSS from files listed in manifest object (currently only geoworld.css)
+var cssSource = "";
+manifest.stylesheets.forEach(function(fileName, index, array) {
+	cssSource = cssSource.concat(fs.readFileSync('src/css/geoworld.css', 'utf8'));
+});
 
 // Minify the CSS code using clean css
-var minifiedCSS = CleanCSS().minify(cssSource);
+var minifiedCSS = cleanCSS().minify(cssSource);
 
 // Write the finalized CSS code to the release directory
 fs.writeFile('release/geoworld.css', minifiedCSS, function(err) {
 	if(err) {
-		console.log("Could not write release/geoworld.css file\n" + err);
+		console.error("Could not write release/geoworld.css file\n" + err);
+		return;
 	}
+	console.log("wrote file: release/geoworld.css");
 });
 
 
@@ -53,10 +54,12 @@ fs.writeFile('release/geoworld.css', minifiedCSS, function(err) {
 fs.readdir('resources/spritesheets', function(err, files) {
 	
 	if(err) {
-		console.log("Could not read from directory resources/spritesheets\n" + err);
+		console.error("Could not read from directory resources/spritesheets\n" + err);
+		return;
 	}
 	
 	files.forEach(function(fileName, index, array) {
 		fs.createReadStream('resources/spritesheets/' + fileName).pipe(fs.createWriteStream('release/' + fileName));
+		console.log("wrote file: release/" + fileName);
 	});
 });
--- watch.js
+++ watch.js
@@ -2,46 +2,32 @@ var http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
 	uglifyJS = require("uglify-js"),
-	cleanCSS = require("clean-css");
+	cleanCSS = require("clean-css"),
+	manifest = require("./manifest.json");
 
 //===========================================
 // JavaScript processing
 //-------------------------------------------
 
-// All javascript files within the geoworld application
-// Be sure to list files in order of inclusion
-// to avoid initialization issues
-var jsFileList = [
-	"src/js/utilities.js",
-	"src/js/animation_polyfill.js",
-	"src/js/math_helper_library.js",
-	"src/js/vector_library.js",
-	"src/js/dynamicPhysicsObject.js",
-	"src/js/player.js",
-	"src/js/geoworld.js"
-];
+// Watch the files in the manifest.javascripts array
+//  and re-process the combined, minified JavaScript any time a change is made
+manifest.javascripts.forEach( function(fileName, index, array) {
 
-// Combine and minify all files in the jsFileList array 
-function processJavaScript() {
-
-	// Combine JavaScript files with uglify-js
-	var minifiedJS = uglifyJS.minify(jsFileList);
-
-	// Write combined, minified, JavaScript file to release directory
-	fs.writeFile('release/geoworld.js', minifiedJS.code, function(err) {
-		if(err) {
-			console.error("Could not write release/geoworld.js file\n" + err);
-		}
-	});
-}
-
-// Watch the files in the jsFileList array, and re-process
-// the combined, minified JavaScript any time a change is made
-jsFileList.forEach( function(fileName, index, array) {
-console.log(fileName);
-	fs.watch(fileName, function(event, filename){
+	fs.watch(fileName, function(event, filename){	
 		console.log(fileName + " changed, reprocessing JavaScripts");
-		processJavaScript();
+		
+		// Combine JavaScript files with uglify-js
+		var minifiedJS = uglifyJS.minify(jsFileList);
+
+		// Write combined, minified, JavaScript file to release directory
+		fs.writeFile('release/geoworld.js', minifiedJS.code, function(err) {
+			if(err) {
+				console.error("Could not write release/geoworld.js file\n" + err);
+				return;
+			}
+			console.log("wrote file: release/geoworld.js");
+		});
+		
 	});
 });
 
@@ -52,30 +38,31 @@ console.log("Watching for changes in JavaScript");
 // CSS processing
 //----------------------------------------------
 
-// All css files within the Geoworld application 
-// (currently just geoworld.css) are minified
-// and placed in the releases directory
-function processCSS() {
+// Watch the files in the manifest.stylesheets array file and re-process it any time changes are made
+manifest.stylesheets.forEach(function(fileName, index, array) {
 
-	// Read CSS from file (currently only geoworld.css)
-	var cssSource = fs.readFileSync('src/css/geoworld.css', 'utf8');
+	fs.watch(fileName, function(event, fileName) {
+		console.log(fileName + " changed, reprocessing " + fileName);
+		
+		// Read CSS from filenames in the manifest
+		var cssSource = "";
+		manifest.stylesheets.forEach(function(fileName, index, array) {
+			cssSource = cssSource.concat(fs.readFileSync('src/css/geoworld.css', 'utf8'));
+		});
 
-	// Minify the CSS code using clean css
-	var minifiedCSS = cleanCSS().minify(cssSource);
+		// Minify the CSS code using clean css
+		var minifiedCSS = cleanCSS().minify(cssSource);
 
-	// Write the finalized CSS code to the release directory
-	fs.writeFile('release/geoworld.css', minifiedCSS, function(err) {
-		if(err) {
-			console.log("Could not write release/geoworld.css file\n" + err);
-		}
+		// Write the finalized CSS code to the release directory
+		fs.writeFile('release/geoworld.css', minifiedCSS, function(err) {
+			if(err) {
+				console.error("Could not write release/geoworld.css file\n" + err);
+				return;
+			}
+			console.log("wrote file: release/geoworld.css");
+		});
 	});
-}
-
-// Watch the CSS file and re-process it any time changes are made
-fs.watch('src/css/geoworld.css', function(event, fileName) {
-    console.log(event);
-	console.log(fileName + " chagned, reprocessing " + fileName);
-	processCSS();
+	
 });
 
 console.log("Watching for changes in CSS");
@@ -85,24 +72,15 @@ console.log("Watching for changes in CSS");
 // Image processing
 //----------------------------------------------
 
-function processSpriteSheets() {
-	// For now just copy all images from the resources directory
-	fs.readdir('resources/spritesheets', function(err, files) {
-		
-		if(err) {
-			console.log("Could not read from directory resources/spritesheets\n" + err);
-		}
-		
-		files.forEach(function(fileName, index, array) {
-			fs.createReadStream('resources/spritesheets/' + fileName).pipe(fs.createWriteStream('release/' + fileName));
-		});
-	});
-};
-
 // Watch the spritesheet directory and copy files as changes are made
 fs.watch('resources/spritesheets', function(event, fileName) {
-	console.log(fileName + " changed, reprocessing " + fileName);
-	fs.createReadStream('resources/spritesheets/' + fileName).pipe(fs.createWriteStream('release/' + fileName));
+
+	// If a file exists, copy it 
+	// note -- removing a file does not delete it from releases!
+	if(fileName !== null) {
+		console.log(fileName + " changed, reprocessing " + fileName);
+		fs.createReadStream('resources/spritesheets/' + fileName).pipe(fs.createWriteStream('release/' + fileName));
+	}
 });
 
 console.log("Watching for changes in spritesheets");
