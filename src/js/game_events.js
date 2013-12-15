2ebc9a1d7be8d8c5f9ef784b804f14ff1ec06058
Xaetrz <jonathank1993@yahoo.com>
2013-12-14 20:27:15 -0600

    Added level Select Screen and menu input handler
    The input handler helps deal with insuring that pressing a button (like
    up or enter) will only count as one press. Seems to be buggy on hitting
    escape which is why the pause menu doesn't work right now. If somebody
    could find or make an actual background image for the level select
    screen, that would be great.

--- src/js/game_events.js
+++ src/js/game_events.js
@@ -4,86 +4,95 @@
 //       ANYBODY WITH EXPERIENCE WITH THIS SHOULD TRY TO FIX THIS CLASS UP
 
 EventController = function(game) {
-	this.specialEvents = {"none": new EmptyEvent(),
-						 "titlescreen":  new TitleScreen(),
-						 "pausescreen":  new PauseScreen(),
-						 "levelfinishedscreen": new FinishLevelScreen(),
-						 "levelselectscreen": new LevelSelectScreen()
-						 };
+	this.events = {"titlescreen":  new TitleScreen(),
+				   "pausescreen":  new PauseScreen(),
+				   "levelfinishedscreen": new FinishLevelScreen(),
+			 	   "levelselectscreen": new LevelSelectScreen(this.levels),
+				   "level": new LevelEvent()
+	              };
 						
 	this.levels = {"level_5_1": new Level(game, level_5_1),
 	               "level_5_3": new Level(game, level_5_3),
 				   "level_5_4": new Level(game, level_5_4)
 				  };
 				 
-	this.currEvent = this.specialEvents["titlescreen"];
-	this.currLevel = null;
+	this.currEvent = this.events["titlescreen"];
 }				 
 
 EventController.prototype.update = function(timeStep, input) {
-	try {
-		// Handle special events
-		if (this.currEvent.selection !== "noevent") {
-			this.currEvent.update(timeStep, input);
-			var selection = this.currEvent.selection;
-			if (selection !== null) {
-				this.currEvent.selection = null;  // Reset selection
-				this.handleSelection(selection);
-			}
+	//try {
+		// Handle events
+		this.currEvent.update(timeStep, input);
+		var selection = this.currEvent.selection;
+		if (selection !== null) {
+			this.currEvent.selection = null;  // Reset selection
+			this.handleSelection(selection);
 		}
-		// Handle level
-		else {
-			// Pause the game
-			if (input.escape) {
-				this.currEvent = this.specialEvents["pausescreen"];
-			}
-			else {
-				this.currLevel.update(timeStep, input);
-				// Handle the end of a level
-				if (this.currLevel.isFinished()) {
-					this.currEvent = this.specialEvents["levelfinishedscreen"];
-				}
-			}
-		}
-	}
-	catch(e) { 
-		console.log(e); 
-		// HACK: Handles some asynchronous behavior I don't know how to deal with properly
-		this.currEvent = this.specialEvents["none"]; 
-	}
+	//}
+	//catch(e) { 
+	//	console.log(e);  
+	//}
 }
 
 // Takes a selection from an event (e.g. "resume" or "quitgame" in PauseScreen) and handles it
 EventController.prototype.handleSelection = function(selection) {
 	if (selection === "startgame") {
-		this.currEvent = this.specialEvents["none"];
-		this.currLevel = this.levels["level_5_1"];
+		this.currEvent = this.events["levelselectscreen"];
+	}
+	else if (selection === "pause") {
+		this.currEvent = this.events["pausescreen"];
 	}
 	else if (selection === "resume") {
-		this.currEvent = this.specialEvents["none"];
+		this.currEvent = this.events["level"];
 	}
 	else if (selection === "quit") {
-		this.currEvent = this.specialEvents["titlescreen"];
-		this.currLevel = null;
+		this.events["level"].level = null;  // Reset level
+		this.currEvent = this.events["titlescreen"];
 	}
-	
+	else if (selection.indexOf("level") != -1) {
+		this.currEvent = this.events["level"];
+		this.currEvent.level = this.levels[selection];
+	}
 }
 
 EventController.prototype.render = function(timeStep, ctx) {
-	try {
-		if (this.currEvent.selection !== "noevent") {
-			this.currEvent.render(timeStep, ctx);
-		}
-		else if (this.currLevel !== null) {
-			this.currLevel.render(timeStep, ctx);
-		}
+	//try {
+		this.currEvent.render(timeStep, ctx);
+	//}
+	//catch(e) {
+	//	console.log(this.currEvent + ": " + e);
+	//}
+}
+
+
+
+//======================================
+// Level event: Handles levels
+//--------------------------------------
+
+LevelEvent = function() {
+	this.selection = null;
+	this.level = null;
+	this.input_handler = new InputHandler(60);
+}
+
+LevelEvent.prototype.update = function(timeStep, input) {
+	this.input_handler.press(input);
+	var canPressEscape = this.input_handler.check(timeStep, "escape");
+
+	this.level.update(timeStep, input);
+	if (this.level.isFinished()) {
+		this.selection = "finishlevel";
 	}
-	catch(e) {
-		console.log(this.currEvent + ": " + e);
-		// HACK: Handles some asynchronous behavior I don't know how to deal with properly
-		this.currEvent = this.specialEvents["none"];
+	else if (canPressEscape) {
+		this.selection = "pause";
 	}
 }
+
+LevelEvent.prototype.render = function(timeStep, ctx) {
+	this.level.render(timeStep, ctx);
+}
+
 
 
 //======================================
@@ -148,14 +157,21 @@ PauseScreen = function() {
 	this.cursor = new Image();
 	this.cursor.src = "cursor.png";
 	
+	this.input_handler = new InputHandler(60);
+	
 	// TODO: MAKE A MORE INTERESTING PAUSE SCREEN
 }
 
 PauseScreen.prototype.update = function(timeStep, input) {
-	if (input.up) this.cursorSelect = Math.clamp(this.cursorSelect - 1, 0, this.numChoices - 1);
-	if (input.down) this.cursorSelect = Math.clamp(this.cursorSelect + 1, 0, this.numChoices - 1);
+	this.input_handler.press(input);
+	var canPressEnter = this.input_handler.check(timeStep, "enter");
+	var canPressUp = this.input_handler.check(timeStep, "up");
+	var canPressDown = this.input_handler.check(timeStep, "down");
+
+	if (canPressUp) this.cursorSelect = Math.clamp(this.cursorSelect - 1, 0, this.numChoices - 1);
+	if (canPressDown) this.cursorSelect = Math.clamp(this.cursorSelect + 1, 0, this.numChoices - 1);
 	
-	if (input.enter) {
+	if (canPressEnter) {
 		if (this.cursorSelect === 0) {
 			this.selection = "resume";
 		}
@@ -182,7 +198,8 @@ PauseScreen.prototype.render = function(timeStep, ctx) {
 // Finish Level Screen
 //--------------------------------------
 FinishLevelScreen = function() {
-
+	this.selection = null;
+	
 }
 
 FinishLevelScreen.prototype.update = function() {
@@ -205,6 +222,18 @@ LevelSelectScreen = function(levels) {
 	// Will needed to be updated for number of levels made for each phase
 	this.numLevels = {"Phase1": 0, "Phase2": 0, "Phase3": 0, "Phase4": 0, "Phase5": 3, 
 					  "Phase6": 0, "Phase7": 0, "Phase8": 0 };
+					  
+	this.cursorSelect = 0;
+	this.menuSelect = 0;  // 0 is for selecting phase, 1 for selecting level number
+	this.phaseSelect = -1;
+	
+	this.top = 50;
+	this.left = 300;
+	this.textYDistance = 45;
+	this.background = new Image();
+	this.background.src = "FutureBackground.png";
+	
+	this.input_handler = new InputHandler(60);
 }
 
 // Takes a level and increases the number of levels completed not completed before
@@ -218,16 +247,76 @@ LevelSelectScreen.prototype.completeLevel = function(level) {
 }
 
 LevelSelectScreen.prototype.update = function(timeStep, input) {
-
+	this.input_handler.press(input);
+	var canPressEnter = this.input_handler.check(timeStep, "enter");
+	var canPressUp = this.input_handler.check(timeStep, "up");
+	var canPressDown = this.input_handler.check(timeStep, "down");
+	
+	// Select phase
+	if (this.menuSelect === 0) {
+		if (canPressUp) this.cursorSelect = Math.clamp(this.cursorSelect - 1, 0, 7);
+		if (canPressDown) this.cursorSelect = Math.clamp(this.cursorSelect + 1, 0, 7);
+		if (canPressEnter && this.numLevels["Phase" + (this.cursorSelect+1)] > 0) {
+			this.phaseSelect = this.cursorSelect+1;
+			this.cursorSelect = 0;
+			this.menuSelect = 1;
+		}
+	}
+	// Select level number
+	else if (this.menuSelect === 1) {
+		if (canPressUp) this.cursorSelect = Math.clamp(this.cursorSelect - 1, 0, this.levelsFinished["Phase" + this.phaseSelect]);
+		if (canPressDown) this.cursorSelect = Math.clamp(this.cursorSelect + 1, 0, this.levelsFinished["Phase" + this.phaseSelect]);
+		if (canPressEnter) {
+			this.selection = "level_" + (this.phaseSelect + "_" + (this.cursorSelect+1));
+		}
+	}
 }
+
 
 LevelSelectScreen.prototype.render = function(timeStep, ctx) {
-
+	ctx.save();
+	ctx.fillStyle = "blue";
+    ctx.font = "bold 25px Arial";
+	
+	// Draw background
+	ctx.drawImage(this.background, 0, 0, 800, 400, 0, 0, 800, 400);
+	
+	// Draw phase choices
+	if (this.menuSelect === 0) {
+		for (i = 1; i <= 8; i++) {
+			// Draw selected phase differently
+			if (this.cursorSelect === i - 1) {
+				ctx.save();
+				ctx.fillStyle = "red";
+				ctx.font = "bold 27px Arial";
+				ctx.fillText("Phase " + i, this.left, this.top + (this.textYDistance * (i-1)));
+				ctx.restore();
+			}
+			// Grey out any phases with no level in them
+			else if (this.numLevels["Phase" + i] === 0) {
+				ctx.save();
+				ctx.fillStyle = "grey";
+				ctx.fillText("Phase " + i, this.left, this.top + (this.textYDistance * (i-1)));
+				ctx.restore();
+			}
+			else ctx.fillText("Phase " + i, this.left, this.top + (this.textYDistance * (i-1)));
+		}
+	}
+	// Draw level choices
+	else if (this.menuSelect === 1) {
+		// Loop through all the available levels in the chosen phase
+		for (i = 1; i <= this.levelsFinished["Phase" + this.phaseSelect] + 1; i++) {
+			// Draw selected level differently
+			if (this.cursorSelect === i - 1) {
+				ctx.save();
+				ctx.fillStyle = "red";
+				ctx.font = "bold 27px Arial";
+				ctx.fillText("Level " + i, this.left, this.top + (this.textYDistance * (i-1)));
+				ctx.restore();
+			}
+			else
+			ctx.fillText("Level " + i, this.left, this.top + (this.textYDistance * (i-1)));
+		}
+	}
+	ctx.restore();
 }
-
-
-// HACK: USED TO (POORLY) DEAL WITH JAVASCRIPT'S ASYNCHRONOUS BEHAVIOR
-EmptyEvent = function() { this.selection = "noevent"; }
-EmptyEvent.prototype.update = function(timeStep, input) {  }
-EmptyEvent.prototype.render = function(timeStep, ctx) {  }
-EmptyEvent.prototype.getSelection = function() { return this.selection; }
