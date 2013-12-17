85095037da0ebd8b82901996b188039fbc4081bf
Grant Borthwick <grantborthwick@gmail.com>
2013-12-16 20:29:01 -0600

    entity files

---
+++ resources/entities/lev_6_1_entities.js
@@ -0,0 +1,8 @@
+/*entities_5_1 = [
+	new Player(-1, new Vector(90,648), new Vector(0,0), -1)
+];
+entities_6_1 = entities_5_1;
+entities_5_2 = entities_5_1;
+entities_5_3 = entities_5_1;
+entities_3_1 = entities_5_1;*/
+entities_6_1 = [];
